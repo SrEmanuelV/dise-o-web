c9ba7211cc7690c65b2d240ef7665fdc9802d1fc
SrEmanuelV <emanuelvillalobos233@gmail.com>
2024-06-07 08:48:27 -0500

    Delete tabla periodica directory

--- tabla periodica/js/main.js
+++
@@ -1,7 +0,0 @@
-function modal(){
-    document.getElementById('modal-image').style.display = 'block';
-}
-
-function cerrar(){
-    document.getElementById('modal-image').style.display = 'none';
-}
